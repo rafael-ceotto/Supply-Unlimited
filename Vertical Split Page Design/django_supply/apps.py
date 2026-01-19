bbe580fed3dcb0aa26ad8e06258f3f7fd7af6d5d
Rafael Ceotto <rafakceotto@hotmail.com>
2026-01-19 16:06:18 +0100

    Remove DOCKER_RUNNING.md and Vertical Split Page Design folder from tracking

--- Vertical Split Page Design/django_supply/apps.py
+++
@@ -1,7 +0,0 @@
-from django.apps import AppConfig
-
-
-class DjangoSupplyConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'django_supply'
-    verbose_name = 'Supply Unlimited'
